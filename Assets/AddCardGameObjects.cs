cb64c04076266de94172348c90e3753123755130
Alexander Lazaris <alex.lazaris617@gmail.com>
2019-06-18 22:35:54 +1000

    First commit

---
+++ Assets/AddCardGameObjects.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCardGameObjects : MonoBehaviour {
+
+	public GameObject [] prefabCards = null;
+	public GameObject [] player1Cards = null;
+	public GameObject [] player2Cards = null;
+	public List<GameObject> [] initialCards = new List<GameObject>[4];
+	public Transform [] player1CardTransforms = null;
+	public Transform [] player2CardTransforms = null;
+	public Transform initialCardTransforms = null;//up to adding initial 4 starting cards, cannot add them it seems ... 
+	public List<string> deckNames = new List<string>(); 
+	public int maxCardsInHands = 6;
+
+	void Start () {
+		CreateCards ();
+	}
+
+	void CreateCards() 
+	{
+		player1Cards = new GameObject[maxCardsInHands]; 
+		player2Cards = new GameObject[maxCardsInHands]; 
+
+		string currentGameObjectName = "";
+		bool matchFound = false;
+		for (int i = 0; i < player1Cards.Length; i++) {
+			matchFound = false;
+			while (!matchFound) {
+				int randomNum = Random.Range (0, prefabCards.Length);
+				currentGameObjectName = prefabCards [randomNum].name;
+				if (!deckNames.Contains (currentGameObjectName)) {
+					deckNames.Add (currentGameObjectName);
+					player1Cards [i] = prefabCards [randomNum];
+					Instantiate (prefabCards [randomNum]);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().SetParent (player1CardTransforms [i].transform);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localPosition = new Vector3 (0, 0, 0);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localRotation = new Quaternion(0, 0, 0, 0);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localScale = new Vector3 (100, 100, 10);
+					matchFound = true;
+				} else {
+					Debug.Log ("Current card: '" + currentGameObjectName + "' is already found"); 
+				}
+			}
+		}
+
+		for (int i = 0; i < player2Cards.Length; i++) {
+			matchFound = false;
+			while (!matchFound) {
+				int randomNum = Random.Range (0, prefabCards.Length);
+				currentGameObjectName = prefabCards [randomNum].name;
+				if (!deckNames.Contains (currentGameObjectName)) {
+					deckNames.Add (currentGameObjectName);
+					player2Cards [i] = prefabCards [randomNum];
+					Instantiate (prefabCards [randomNum]);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().SetParent (player2CardTransforms [i].transform);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localPosition = new Vector3 (0, 0, 0);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localRotation = new Quaternion(0, 0, 0, 0);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localScale = new Vector3 (100, 100, 10);
+					matchFound = true;
+				} else {
+					Debug.Log ("Current card: '" + currentGameObjectName + "' is already found"); 
+				}
+			}
+		}
+
+		for (int i = 0; i < initialCards.Length; i++) {
+			matchFound = false;
+			while (!matchFound) {
+				int randomNum = Random.Range (0, prefabCards.Length);
+				currentGameObjectName = prefabCards [randomNum].name;
+				if (!deckNames.Contains (currentGameObjectName)) {
+					deckNames.Add (currentGameObjectName);
+					initialCards [i].Add(prefabCards [randomNum]);//look up how to add gameobjects to Lists in Unity
+					Instantiate (prefabCards [randomNum]);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().SetParent (initialCardTransforms.transform);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localPosition = new Vector3 (0, 0, ((i + 1) * Time.deltaTime));
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localRotation = new Quaternion(0, 0, 0, 0);
+					GameObject.Find (currentGameObjectName + "(Clone)").GetComponent<Transform> ().gameObject.transform.localScale = new Vector3 (100, 100, 10);
+					matchFound = true;
+				} else {
+					Debug.Log ("Current card: '" + currentGameObjectName + "' is already found"); 
+				}
+			}
+		}
+	}
+}
---
+++ Assets/BasicGameplay.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BasicGameplay : MonoBehaviour 
+{
+	[SerializeField]
+	Text player1ScoreText = null;
+	[SerializeField]
+	Text player2ScoreText = null;
+	[SerializeField]
+	Text initialDealCardsText = null;
+
+	public int Player1ScoreCount = 0;
+	public int player2ScoreCount = 0;
+	public int totalCardsInDeck = 52;
+	public static string [] player1CardsInHands = null;
+	public static string [] player2CardsInHands = null;
+	public int maxCardsInHands = 6;
+	string [] deck = new string[13] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+	public int [] deckCount = new int[13] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	public string [] initialDealCards = new string[4];
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		Deal ();
+	}
+
+	public int getPlayer1CardsLength()
+	{
+		player1CardsInHands = new string[maxCardsInHands];
+		return player1CardsInHands.Length;
+	}
+
+	public void Deal ()
+	{
+		player1ScoreText.text = "";
+		player2ScoreText.text = "";
+		initialDealCardsText.text = "";
+
+		player1CardsInHands = new string[maxCardsInHands];
+		player2CardsInHands = new string[maxCardsInHands];
+		string dealtCard = "";
+		bool player1Dealt = false;
+		bool player2Dealt = false;
+		bool gamestartDealt = false;
+
+		/*for (int i = 0; i < maxCardsInHands; i++) 
+		{
+			
+
+			player1Dealt = false;
+			player2Dealt = false;
+			while (!player1Dealt && !player2Dealt) 
+			{
+				
+				if (!player1Dealt) {
+					int randomNum = Random.Range (0, 13);
+					if (!(deckCount [randomNum] >= 4)) {
+						player1Dealt = true;
+						deckCount [randomNum] += 1;
+						dealtCard = deck [randomNum];
+						Debug.Log ("Card is " + dealtCard + " and has appeared " + deckCount [randomNum] + " times"); 				
+						player1CardsInHands [i] = dealtCard;
+					}
+				}
+				if (!player2Dealt) {
+					int randomNum = Random.Range (0, 13);
+					if (!(deckCount [randomNum] >= 4)) {
+						player2Dealt = true;
+						deckCount [randomNum] += 1;
+						dealtCard = deck [randomNum];
+						Debug.Log ("Card is " + dealtCard + " and has appeared " + deckCount [randomNum] + " times"); 				
+						player2CardsInHands [i] = dealtCard;
+					}
+				}
+			}
+			string player1 = player1CardsInHands [i];
+			player1ScoreText.text += "," + player1;
+			string player2 = player2CardsInHands [i];
+			player2ScoreText.text += "," + player2;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			gamestartDealt = false;
+			if (!gamestartDealt) {
+				int randomNum = Random.Range (0, 13);
+				if (!(deckCount [randomNum] >= 4)) {
+					gamestartDealt = true;
+					deckCount [randomNum] += 1;
+					dealtCard = deck [randomNum];
+					Debug.Log ("Card is " + dealtCard + " and has appeared " + deckCount [randomNum] + " times"); 				
+					initialDealCards [i] = dealtCard;
+				}
+			}
+			string temp = initialDealCards [i];
+			if (i == 4) {
+				initialDealCardsText.text += temp;
+			}
+			else
+				initialDealCardsText.text += temp + ",";
+		}*/
+	}
+}
